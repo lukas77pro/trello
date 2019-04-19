7ca940ea1f3cedf63ffbb495387b9459a5a6531e
tirey93 <tirey93@gmail.com>
2019-04-20 00:23:31 +0200

    Added annotatation that allows to login trello trello

--- trello-server/src/main/java/pl/trello/config/WebMvcConfig.java
+++ trello-server/src/main/java/pl/trello/config/WebMvcConfig.java
@@ -3,7 +3,7 @@ package pl.trello.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
