5e47329d796197b9489d4e3f7aa6d8c8c0844821
ogor1992 <ogor1992@gmail.com>
2019-06-07 10:59:28 +0200

    Add swPush handling, notification subscription

--- trello-app/src/app/app.component.ts
+++ trello-app/src/app/app.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/service/auth.service';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <button class="button button-primary" (click)="subscribeToNotifications()">
+    Subscribe
+  </button>`,
+  //templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
-  constructor(private authService: AuthService) {
-  }
+  readonly VAPID_PUBLIC_KEY = "BOqo8P-LKyC-Jf6M1TD3QQhxUDMVFNydhrsJJORsnIYbgr9ESPMjGK2kFErWvswcLsC1hFSmutwg7N1AkG_jwNY";
+
+  constructor(
+    private authService: AuthService,
+    private swPush: SwPush,
+    /*private newsletterService: NewsletterService*/) {}
 
   ngOnInit(): void {
     this.authService.loadUser();
   }
+
+  subscribeToNotifications() {
+    this.swPush.requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+    /*.then(sub => this.newsletterService.addPushSubscriber(sub).subscribe())*/
+    .catch(err => console.error("Could not subscribe to notifications", err));
+  }
+
 }
